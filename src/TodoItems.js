5211ba0943637f0ddbcc312d2ee4e5e0d2a0dcc4
sonihemant <sonihemant8094@gmail.com>
2021-02-10 08:52:52 +0530

    it is a TodoList app

---
+++ src/TodoItems.js
@@ -0,0 +1,39 @@
+import React,{Component} from "react";
+
+
+class TodoItems extends Component {
+
+    constructor(props) {
+        super(props);
+
+
+        this.createTasks = this.createTasks.bind(this);
+    }
+
+    delete(key){
+        this.props.delete(key);
+    }
+
+    createTasks(item) {
+        return <li onClick ={()=> this.delete(item.key)} key= {item.key}>{item.text} </li>
+    }
+    
+    render() {
+        var todoEntries = this.props.entries;
+        
+        var ListItem = todoEntries.map(this.createTasks);
+
+        return (
+            <ul className = "theList">
+                {ListItem}
+            </ul>
+        );
+    };
+
+
+
+
+
+};
+
+export default TodoItems;
